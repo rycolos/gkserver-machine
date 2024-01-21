5ff26160e797c1a48ec61177241dadc53b5ecddc
Ryan LaLiberty <rycolos@gkserver.net>
2024-01-21 16:12:34 -0500

    deprecated cctv retention policy from trash_clean script

--- scripts/trash_clean.sh
+++ scripts/trash_clean.sh
@@ -2,16 +2,13 @@
 
 #SET RETENTION PERIODS
 days=30
-days2=60
 
 #SET DIRECTORIES
 logdest="/home/kepler/logs/retention.log"
 logsrc="/home/kepler/logs"
 trashsource1="/media/backup_main/plex_library/plex_library_trash"
 trashsource2="/media/backup_main/gkserver_home/gkserver_home_trash"
-#trashsource3="/media/backup_main/docker-data/docker-data_trash/$(date +%m-%d-%Y)"
 trashsource3="/media/backup_main/docker-data/docker-data_trash"
-trashsource4="/media/backup_main/cctv/"
 
 echo $(date) >> $logdest
 echo "" >> $logdest
@@ -23,9 +20,6 @@ find $logsrc -mtime +$days -delete -print 2>&1 | tee -a $logdest
 find $trashsource1 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee -a $logdest
 find $trashsource2 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee -a $logdest
 find $trashsource3 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee -a $logdest
-
-#prune cctv trash after 60d
-find $trashsource4 -mindepth 1 -mtime +$days2 -exec rm -rv {} + -print 2>&1 | tee -a $logdest
 
 echo "" >> $logdest
 echo $(date) >> $logdest
