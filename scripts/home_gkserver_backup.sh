9fea47d23c435ae7e1e3cd8d9a3c4b4d09f07fab
Ryan LaLiberty <rycolos@gkserver.net>
2024-01-21 08:42:34 -0500

    home bakcup testing

--- scripts/home_gkserver_backup.sh
+++ scripts/home_gkserver_backup.sh
@@ -4,13 +4,15 @@
 logdest="/home/kepler/logs/home_gkserver_backup.log"
 src1="/home/kepler"
 dest1="/media/backup_main/gkserver_home"
-exclude1="/home/kepler/gkserver"
+exclude1="gkserver"
 trashdir="/media/backup_main/gkserver_home/gkserver_home_trash/$(date +%m-%d-%Y)"
 
 echo $(date) >> $logdest
 echo "" >> $logdest
 
 rsync -avhi --delete --exclude $exclude1 --backup-dir=$trashdir $src1 $dest1 2>&1 | tee -a $logdest
+
+#rsync -avhi --delete --exclude /home/kepler/gkserver /home/kepler /media/backup_main/gkserver_home --dry-run
 
 echo "" >> $logdest
 echo $(date) >> $logdest
