4efbe9d0bfb87ffb50fb08323821b8d21bf72a47
Ryan LaLiberty <rycolos@gkserver.net>
2024-09-18 10:15:55 -0400

    trash clean update for hosaka

--- scripts/trash_clean.sh
+++ scripts/trash_clean.sh
@@ -5,33 +5,35 @@ days=14
 
 #SET DIRECTORIES
 user="ryan"
+
 logdir="/home/$user/logs"
-logdest="/home/$user/logs/retention.log"
+logdest="retention.log"
+
 logsrc="/home/$user/logs"
 vol1="/mnt/backup_int1"
 
-trashsource1="$vol1/backup_trash"
+trashsource1="$vol1/library_trash"
 trashsource2="$vol1/docker_data/docker_data_trash"
 
 mkdir -p $logdir
-echo $(date) >> $logdest
-echo "" >> $logdest
+echo $(date) >> $logdir/$logdest
+echo "" >> $logdir/$logdest
 
 #verify vol1 is mounted, exit if not
 if grep -qs $vol1 /proc/mounts; then
-    echo "$vol1 mounted" >> $logdest
+    echo "$vol1 mounted" >> $logdir/$logdest
 else
-    echo "$vol1 not mounted" >> $logdest
+    echo "$vol1 not mounted" >> $logdir/$logdest
     exit 1
 fi
 
 #prune logs after DAYS
-find $logsrc -name "*.log" -mtime +$days -delete -print 2>&1 | tee $logdest
+find $logsrc -name "*.log" -mtime +$days -delete -print 2>&1 | tee $logdir/$logdest
 
 #prune backup_trash folders after DAYS
-find $trashsource1 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee $logdest
-find $trashsource2 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee $logdest
+find $trashsource1 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee $logdir/$logdest
+find $trashsource2 -maxdepth 1 -mindepth 1 -mtime +$days -type d -exec rm -rv {} + -print 2>&1 | tee $logdir/$logdest
 
-echo "" >> $logdest
-echo $(date) >> $logdest
-echo "----------" >> $logdest
+echo "" >> $logdir/$logdest
+echo $(date) >> $logdir/$logdest
+echo "----------" >> $logdir/$logdest
