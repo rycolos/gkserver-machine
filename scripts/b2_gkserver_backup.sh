6cbbb78fb5ec56c89374871c07208103ab64c7b1
Ryan LaLiberty <rycolos@gkserver.net>
2024-09-18 14:36:42 -0400

    b2 - remove colons

--- scripts/b2_gkserver_backup.sh
+++ scripts/b2_gkserver_backup.sh
@@ -5,7 +5,7 @@ user="ryan"
 
 mnt1="/mnt/backup_int1"
 mnt2="/mnt/syncthing"
-remote="b2:/gkserver-backup"
+remote="b2:gkserver-backup"
 
 logdir="/home/$user/logs"
 logdest="b2_gkserver_backup.log"
@@ -19,27 +19,27 @@ echo "" >> $logdir/$logdest
 
 #SYNC
     #docker-data
-rclone sync $mnt1/docker_data $remote:/docker_data \
+rclone sync $mnt1/docker_data $remote/docker_data \
 --config=$config \
 --fast-list --progress --transfers 20 2>&1 | tee $logdir/$logdest
 
     #other-backups
-rclone sync $mnt1/other_backups $remote:/other_backups \
+rclone sync $mnt1/other_backups $remote/other_backups \
 --config=$config \
 --fast-list --progress --transfers 20 2>&1 | tee $logdir/$logdest
 
     #archives
-rclone sync $mnt1/library_backup/archives $remote:/archives \
+rclone sync $mnt1/library_backup/archives $remote/archives \
 --config=$config \
 --fast-list --progress --transfers 20 2>&1 | tee $logdir/$logdest
 
     #music
-rclone sync $mnt1/library_backup/music $remote:/music \
+rclone sync $mnt1/library_backup/music $remote/music \
 --config=$config \
 --fast-list --progress --transfers 20 2>&1 | tee $logdir/$logdest
 
     #syncthing
-rclone sync $mnt2/ $remote:/syncthing \
+rclone sync $mnt2/ $remote/syncthing \
 --config=$config \
 --fast-list --progress --transfers 20 2>&1 | tee $logdir/$logdest
 
